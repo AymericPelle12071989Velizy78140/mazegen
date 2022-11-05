6cc103eab49e44caa93f12993015401efccd8347
Aymeric Pellé <aympelle@gmail.com>
2022-11-05 15:44:50 +0100

    v0.12: Add pxconv functions _f for float result, to_px().

--- pxconv.py
+++ pxconv.py
@@ -2,12 +2,20 @@ from parse import with_pattern, parse
 from tkinter import Tk
 
 
-def cm_to_px(dist):
+def cm_to_px_f(dist):
     return dist * Tk().winfo_fpixels('1c')
 
 
-def mm_to_px(dist):
+def cm_to_px(dist):
+    return int(cm_to_px_f(dist))
+
+
+def mm_to_px_f(dist):
     return (dist * Tk().winfo_fpixels('1c')) / 10
+
+
+def mm_to_px(dist):
+    return int(mm_to_px_f(dist))
 
 
 def px_to_cm(dist):
@@ -25,7 +33,7 @@ def parse_unit(text):
     return None
 
 
-def str_to_px(value: str):
+def str_to_px_f(value: str):
     res = parse("{len:g}{unit:>Unit}", value, dict(Unit=parse_unit))
     if res is None:
         return None
@@ -33,8 +41,26 @@ def str_to_px(value: str):
     unit = res["unit"]
     match unit:
         case "cm":
-            return cm_to_px(float(length))
+            return cm_to_px_f(float(length))
         case "mm":
-            return mm_to_px(float(length))
+            return mm_to_px_f(float(length))
         case "px" | "":
             return float(length)
+
+
+def str_to_px(value: str):
+    return int(str_to_px_f(value))
+
+
+def to_px_f(value):
+    match value:
+        case int():
+            return value
+        case str():
+            return str_to_px_f(value)
+        case _:
+            return str_to_px_f(str(value))
+
+
+def to_px(value):
+    return int(to_px_f(value))
