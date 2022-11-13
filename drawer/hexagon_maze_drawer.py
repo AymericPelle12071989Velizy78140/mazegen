3f7a3d1d515d1b1024ff54c98dba7c894deb906d
Aymeric Pellé <aympelle@gmail.com>
2022-11-14 00:16:32 +0100

    v0.16: HexagonalSquares are drawn on maze image.

--- drawer/hexagon_maze_drawer.py
+++ drawer/hexagon_maze_drawer.py
@@ -1,24 +1,39 @@
+from pathlib import Path
+
+from wand.color import Color
 from wand.drawing import Drawing
 from wand.image import Image
 
 import hexa_tiling
 import image_toolkit
 from data.hexagon_maze import HexagonMaze
+from data.hexagonal_square import HexagonalSquare
 from drawer.maze_drawer_base import MazeDrawerBase
+from grid.types import GridPosition
+from hexa_pixel_mask import HexaPixelMask
+from vec2i import Vec2i
 
 
 class HexagonMazeDrawer(MazeDrawerBase):
     def __init__(self):
         super().__init__()
         self._tiling = hexa_tiling.HexaTiling(self.square_size * 2)
+        self._hexa_pixel_mask = HexaPixelMask(self._tiling.hexagon_width())
+
+    def load_from_jstruct(self, jstruct, rsc_dirpath: Path = None):
+        super().load_from_jstruct(jstruct, rsc_dirpath)
+        assert self.square_size % 2 == 0
+        self._tiling = hexa_tiling.HexaTiling(self.square_size * 2)
+        self._hexa_pixel_mask = HexaPixelMask(self._tiling.hexagon_width())
 
     def create_maze_image(self, maze: HexagonMaze):
         maze_img = self.__create_empty_maze_image(maze)
+        self._draw_squares(maze, maze_img)
         return maze_img
 
     def __create_empty_maze_image(self, maze: HexagonMaze):
         image_dim = self._tiling.surface_dimension((maze.width, maze.height))
-        maze_img = Image(width=image_dim.x, height=image_dim.y, background=self.background_color)
+        maze_img = Image(width=image_dim.x, height=image_dim.y, background=Color("#00000000"))
         if self.background_image_path:
             self._draw_background(maze_img, image_dim)
         return maze_img
@@ -28,3 +43,36 @@ class HexagonMazeDrawer(MazeDrawerBase):
             bg_img = image_toolkit.load_image(str(self.background_image_path), image_dim.x, image_dim.y)
             draw.composite(operator='over', left=0, top=0, width=image_dim.x, height=image_dim.y, image=bg_img)
             draw(maze_img)
+
+    def _draw_squares(self, maze: HexagonMaze, maze_img: Image):
+        for j in range(maze.height):
+            for i in range(maze.width):
+                tile_pos = GridPosition(i, j)
+                h_square = maze[tile_pos]
+                if h_square is not None:
+                    assert isinstance(h_square, HexagonalSquare)
+                    self._draw_square(h_square, tile_pos, maze_img)
+
+    def _draw_square(self, square: HexagonalSquare, tile_pos: GridPosition, maze_img: Image):
+        sq_ground = square.ground
+        if isinstance(sq_ground, str) and len(sq_ground) > 0:
+            ground_img: Image = self.ground_dict[sq_ground]
+            self._draw_on_square(ground_img, tile_pos, maze_img)
+        sq_type = square.type
+        if isinstance(sq_type, str) and len(sq_type) > 0:
+            type_img: Image = self.square_dict[sq_type]
+            self._draw_on_square(type_img, tile_pos, maze_img)
+
+    def _draw_on_square(self, image: Image, t_pos: GridPosition, maze_img: Image):
+        tile_img_pos = self._tiling.tile_pos(t_pos)
+        with Drawing() as draw:
+            draw.composite(operator='over',
+                           left=tile_img_pos.x, top=tile_img_pos.y,
+                           width=image.width, height=image.height, image=image)
+            draw(maze_img)
+
+    def _filter_image(self, image):
+        self._hexa_pixel_mask.crop_image(image)
+
+    def _square_image_size(self):
+        return self._tiling.hexagon_width()
